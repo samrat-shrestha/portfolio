fa3fd8d47797cbfefda265aee358ac6fc21b672f
Samrat Shrestha <samstha007@gmail.com>
2019-09-23 20:55:10 +0545

    Jobs app up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# models.Model allows job class to be connected to the database.
+class Job(models.Model):
+	image = models.ImageField(upload_to='images/')
+	summary = models.CharField(max_length=200)
+
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,6 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'portfoliodb',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'samrat12345',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
@@ -117,4 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
